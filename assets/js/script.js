e13b72d04799d16979da26d1c7cbf7efd18cc958
LauraMayock <mcfadden.laura@yahoo.com>
2022-07-02 21:06:32 +0000

    Add media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,31 @@ let computerScore = 1;
 let playerScore = 1;
 
 //font awsome icons 
-const rockIcon = "fas fa-hand-rock";
-const paperIcon = "fas fa-hand-paper";
-const scissorsIcon = "fas fa-hand-scissors";
+let rockIcon = "fas fa-hand-rock";
+let paperIcon = "fas fa-hand-paper";
+let scissorsIcon = "fas fa-hand-scissors";
+let spockIcon = "fa-solid fa-hand-spock";
+let lizardIcon = "fa-solid fa-hand-lizard";
 
-const randomClasses = [rockIcon, paperIcon, scissorsIcon];
-const text = document.getElementById('demo');
-const text2 = document.getElementById('demo2');
+
+const randomClasses = [rockIcon, paperIcon, scissorsIcon, spockIcon, lizardIcon];
+// comment on outcome of on each round
+const text = document.getElementById('text');
+const text2 = document.getElementById("text2");
 
 const tie = ()=>{
     text.innerHTML = "It's a Tie ! ";
     text.style.color = 'orange';
-    text2.innerHTML = text.innerHTML;
-    text2.style.color = 'orange';
 }
 
 const win = ()=>{
     text.innerHTML = "It's a Win ! ";
     text.style.color = 'rgb(1, 146, 1)';
-    text2.innerHTML = text.innerHTML;
-    text2.style.color = 'rgb(1, 146, 1)';
 }
 
 const lose = ()=>{
-    text.innerHTML = "You Loosed ! ";
+    text.innerHTML = "You Lost ! ";
     text.style.color = 'red';
-    text2.innerHTML = text.innerHTML;
-    text2.style.color = 'red';
 }
 
 // Game Functionality.
@@ -60,27 +58,104 @@ const game = () =>{
            else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
+               text2.innerHTML = "Rock crushes Scissors";
                win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === lizardIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               text2.innerHTML = "Rock crushes Lizard";
+               win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === spockIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               text2.innerHTML = "Spock vaporizes Rock";
+               lose();
            }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               text2.innerHTML = "Paper covers Rock";
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === spockIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               lose();
-           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               lose();
+               text2.innerHTML = "Paper disproves Spock";
+               win();
            }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
+               text2.innerHTML = "Paper covers Rock";
                win();
-           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               lose();
-           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               win();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Scissors cuts Paper";
+                playerScore++;
+                lose();
+            }else if(showIcon.className === paperIcon && computerShowIcon.className === lizardIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Lizard eats Paper";
+                computerScore++;
+                lose();
+            }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Scissors cuts Paper";
+                computerScore++;
+                win();
+            }else if(showIcon.className === scissorsIcon && computerShowIcon.className === lizardIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Scissors decapitates Lizard";
+                playerScore++;
+                win();
+            }else if(showIcon.className === scissorsIcon && computerShowIcon.className === spockIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Spock smashes Scissors";
+                computerScore++;
+                lose();
+            }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Rock crushes Scissors";
+                playerScore++;
+                lose();
+            }else if(showIcon.className === spockIcon && computerShowIcon.className === rockIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Spock vaporizes Rock";
+                computerScore++;
+                win();
+            }else if(showIcon.className === spockIcon && computerShowIcon.className === scissorsIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Spock smashes Scissors";
+                playerScore++;
+                win();
+            }else if(showIcon.className === spockIcon && computerShowIcon.className === paperIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Paper disproves Spock";
+                computerScore++;
+                lose();
+            }else if(showIcon.className === spockIcon && computerShowIcon.className === lizardIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Lizard poisons Spock";
+                computerScore++;
+                lose();
+            }else if(showIcon.className === lizardIcon && computerShowIcon.className === paperIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Lizard eats Paper";
+                playerScore++;
+                win();
+            }else if(showIcon.className === lizardIcon && computerShowIcon.className === spockIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Lizard poisons Spock";
+                computerScore++;
+                win();
+            }else if(showIcon.className === lizardIcon && computerShowIcon.className === rockIcon){
+                pScore.innerHTML = playerScore;
+                text2.innerHTML = "Rock crushes Lizard";
+                playerScore++;
+                lose();
+            }else if(showIcon.className === lizardIcon && computerShowIcon.className === scissorsIcon){
+                cScore.innerHTML = computerScore;
+                text2.innerHTML = "Scissors decapitates Lizard";
+                computerScore++;
+                lose();
+
            }
         });
     });
